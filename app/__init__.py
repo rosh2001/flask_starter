947383b40ea82ca80726409b453f7503fc6f52a0
rosh2001 <91378243+rosh2001@users.noreply.github.com>
2022-03-19 18:51:55 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+from flask_migrate import Migrate
+migrate =  Migrate(app,db)
 from app import views
